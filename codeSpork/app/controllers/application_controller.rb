32a4633f5bd9aaac234d9227b561f2bd9a31dfae
Richard Williams <coldheart042@gmail.com>
2014-02-11 21:58:12 -0800

    Fixed edit registration issue

--- codeSpork/app/controllers/application_controller.rb
+++ codeSpork/app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :email, :password, :password_confirmation, :avatar, :signature) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:user_name, :email, :password, :password_confirmation, :avatar, :signature) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:user_name, :email, :password, :password_confirmation, :current_password, :avatar, :signature) }
   end
 end
