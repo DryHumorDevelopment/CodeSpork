c8b76166312a2169db01eb35f85f38755485c89b
justkrys1 <justkrys1@gmail.com>
2014-02-16 12:03:26 -0800

    Avatar
    Added carrierwave gem, generated uploader, added file upload to new and
    edit registration

--- codeSpork/app/models/user.rb
+++ codeSpork/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  #attr_accessible :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  mount_uploader :avatar, ImageUploaderUploader
 end
