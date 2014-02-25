206a1f63ffad9c1fbfaaf1b3cbcdf6bf9300c306
Brandon Garcia-Acain <brandonkeoni@gmail.com>
2014-02-24 20:23:17 -0800

    Changes made that were necessary for deployment.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require tinymce-jquery
+//= require_tree
--- app/assets/javascripts/main.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    $("#tabs").tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
-    $(".accordion").accordion({collapsible: true, active: false});
-});
